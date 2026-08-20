e47d35bfb84c09ab1b5a684a807570ce23088168
Mahima-Sanketh-Git <mahimadevops@gmail.com>
2026-08-20 20:19:59 +0530

    add core organization attribute constants

---
+++ features/admin.registration-flow-builder.v1/constants/organization-attribute-constants.ts
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2026, WSO2 LLC. (https://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { OrganizationAttribute } from "../models/attributes";
+
+/**
+ * Constants related to organization attributes in the Registration flow.
+ *
+ * @remarks
+ * This class is not meant to be instantiated. It only provides static constants.
+ *
+ * @example
+ * ```typescript
+ * const attributes = OrganizationAttributeConstants.CORE_ATTRIBUTES;
+ * ```
+ */
+class OrganizationAttributeConstants {
+    /**
+     * Private constructor to avoid object instantiation from outside the class.
+     */
+    private constructor() {}
+
+    /**
+     * Core organization attributes offered by the attribute selector.
+     *
+     * `organizationHandle` is deliberately absent — the dedicated `ORG_HANDLE` input variant
+     * owns that identifier, and the attribute selector is not rendered for that variant.
+     */
+    public static readonly CORE_ATTRIBUTES: OrganizationAttribute[] = [
+        {
+            claimURI: "organizationName",
+            dataType: "STRING",
+            description: "Name of the organization",
+            displayName: "Organization Name",
+            required: true
+        },
+        {
+            claimURI: "organizationDescription",
+            dataType: "STRING",
+            description: "Description of the organization",
+            displayName: "Organization Description",
+            required: false
+        }
+    ];
+}
+
+export default OrganizationAttributeConstants;
